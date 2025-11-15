6e3d18a9581adae0eb705a577f5f324362d7903a
modicl <fe.villarroelb@duocuc.cl>
2025-11-15 13:47:04 -0300

    UPDATE : Configuracion cors

--- src/main/java/cl/huertohogar/usuario_backend/config/WebConfig.java
+++ src/main/java/cl/huertohogar/usuario_backend/config/WebConfig.java
@@ -2,6 +2,7 @@ package cl.huertohogar.usuario_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(roleCheckInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://huertohogar.nyc3.cdn.digitaloceanspaces.com")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
